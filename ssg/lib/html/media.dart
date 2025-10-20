ec3397313a10e9e537c5b8de301b085dc7f785a7
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2025-10-20 05:36:36 +0200

    Automatic image sizing

--- ssg/lib/html/media.dart
+++ ssg/lib/html/media.dart
@@ -1,9 +1,16 @@
+import "dart:io";
+
+import "package:image/image.dart" as img;
 import "package:ssg/html/base.dart";
 import "package:ssg/utils.dart";
 
 class Image extends Element {
   String src;
   String alt;
+  int? width;
+  int? height;
+
+  bool autoSize;
 
   Image({
     required this.src,
@@ -11,10 +18,40 @@ class Image extends Element {
     super.id,
     super.classes,
     super.inlineStyles,
+    this.width,
+    this.height,
+    this.autoSize = true,
   }) : super(children: []);
 
   @override
   String build() {
-    return '<img src="$src" alt="${alt.escape()}"$modifiers>';
+    return '<img src="$src" alt="${alt.escape()}"$imageSize$modifiers>';
+  }
+
+  String get imageSize {
+    if (width != null && height != null) {
+      return " width=$width height=$height";
+    }
+    if (width != null) {
+      return " width=$width";
+    }
+    if (height != null) {
+      return " height=$height";
+    }
+    if (autoSize) {
+      final file = File(src);
+      if (file.existsSync()) {
+        final img.Image? imageData = img.decodeImage(file.readAsBytesSync());
+        if (imageData == null) return "";
+        inlineStyles = [
+          "max-width: 100%",
+          "height: auto",
+          "max-height: ${imageData.height}px",
+          ...inlineStyles ?? [],
+        ];
+        return " width=${imageData.width} height=${imageData.height}";
+      }
+    }
+    return "";
   }
 }
