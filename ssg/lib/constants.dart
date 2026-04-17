14569029dae314796e31ebb155f437c1fcda4c1f
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-04-17 19:12:34 +0200

    Download Visuals with correct filenames

--- ssg/lib/constants.dart
+++ ssg/lib/constants.dart
@@ -10,7 +10,7 @@ final github = GitHub(auth: findAuthenticationFromEnvironment());
 HttpClient http = HttpClient()..userAgent = "TechnicJelle's SSG";
 
 extension UriExtension on Uri {
-  String getFileName() => p.basename(path);
+  String getFileName() => p.basename(pathSegments.join("/"));
 
-  String getExtension() => p.extension(path);
+  String getExtension() => p.extension(pathSegments.join("/"));
 }
